df0190fc6a051afa633d9195ad54d0108fa6fa63
Sherlock Yiu <yiu.cheuk.lok@gmail.com>
2024-03-06 01:04:58 +0800

    chore: fine tuning colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -148,28 +148,36 @@ module.exports = {
         addComponents({
           ".bg-glass": {
             '.dark &': {
-              background: "rgba(255, 255, 255, 0.15)",
+              background: "radial-gradient(farthest-corner at top left, rgb(69 69 69 / 40%) 20%, rgb(29 29 29 / 70%))",
               border: "1px solid rgba(255, 255, 255, 0.1)",
-              "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.1)",
+              "box-shadow": "inset -1px -1px 2px rgb(155 192 155 / 10%), inset -1px -2px 12px rgb(52 52 52 / 20%)",
             },
             background: "rgba(255, 255, 255, 0.1)",
             border: "1px solid rgba(0, 0, 0, 0.1)",
             "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.05)",
             "border-radius": "16px",
-            "backdrop-filter": "blur(5px)",
+            "backdrop-filter": "blur(6px)",
           },
           ".bg-deep-glass": {
             '.dark &': {
-              background: "rgba(255, 255, 255, 0.05)",
+              background: "radial-gradient(farthest-corner at top left, rgb(29 29 29 / 100%) 30%, rgb(21 21 21 / 100%))",
               border: "1px solid rgba(255, 255, 255, 0.1)",
-              "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.1)",
+              "box-shadow": "inset -1px -1px 2px rgb(155 192 155 / 10%), inset -1px -2px 16px rgb(0 0 0 / 40%)",
             },
-            background: "rgba(255, 255, 255, 0.3)",
-            border: "1px solid rgba(0, 0, 0, 0.1)",
-            "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.05)",
             "border-radius": "16px",
-            "backdrop-filter": "blur(16px)",
           },
+          // ".bg-deep-glass": {
+          //   '.dark &': {
+          //     background: "rgba(255, 255, 255, 0.05)",
+          //     border: "1px solid rgba(255, 255, 255, 0.1)",
+          //     "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.1)",
+          //   },
+          //   background: "rgba(255, 255, 255, 0.3)",
+          //   border: "1px solid rgba(0, 0, 0, 0.1)",
+          //   "box-shadow": "0 4px 16px rgba(0, 0, 0, 0.05)",
+          //   "border-radius": "16px",
+          //   "backdrop-filter": "blur(16px)",
+          // },
         });
         matchUtilities(
           {
