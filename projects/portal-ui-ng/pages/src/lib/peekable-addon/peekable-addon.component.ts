9ba3c805bb680ab6a13302c9dcc1f307a307754e
Sherlock Yiu <1911214+sheryiu@users.noreply.github.com>
2025-04-27 22:33:31 +0800

    feat: escape key can close peekable addon now

--- projects/portal-ui-ng/pages/src/lib/peekable-addon/peekable-addon.component.ts
+++ projects/portal-ui-ng/pages/src/lib/peekable-addon/peekable-addon.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, inject } from '@angular/core';
+import { OverlayKeyboardDispatcher, OverlayRef } from '@angular/cdk/overlay';
+import { Component, computed, DestroyRef, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonModule, } from 'portal-ui-ng/base';
 import { TooltipDirective } from 'portal-ui-ng/components';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subject } from 'rxjs';
 import { PEEKABLE_ADDON_DATA_PROVIDER } from './peekable-addon';
 
 @Component({
@@ -17,14 +18,18 @@ import { PEEKABLE_ADDON_DATA_PROVIDER } from './peekable-addon';
   templateUrl: './peekable-addon.component.html',
   styles: ``,
   host: {
-    class: 'pui-peekable-addon'
+    class: 'pui-peekable-addon',
   }
 })
 export class PeekableAddonComponent {
   private dataProvider = inject(PEEKABLE_ADDON_DATA_PROVIDER, { optional: true });
   private route = inject(ActivatedRoute);
+  private router = inject(Router)
+  private overlayKeyboardDispatcher = inject(OverlayKeyboardDispatcher)
 
   protected routeToFullContent = computed(() => this.dataProvider?.routeToFullContent() ?? null)
+
+  readonly _keydownEvents = new Subject<KeyboardEvent>();
 
   constructor() {
     combineLatest([
@@ -33,5 +38,25 @@ export class PeekableAddonComponent {
     ]).pipe(
       takeUntilDestroyed(),
     ).subscribe(([p, qp]) => this.dataProvider?.onParamsChange?.(p, qp))
+
+    this.overlayKeyboardDispatcher.add(this as unknown as OverlayRef)
+    inject(DestroyRef).onDestroy(() => {
+      this.overlayKeyboardDispatcher.ngOnDestroy();
+      this._keydownEvents.complete();
+    })
+
+    this._keydownEvents.pipe(
+      takeUntilDestroyed()
+    ).subscribe(event => {
+      if (event.key == 'Escape') {
+        this.onEscapePress(event)
+      }
+    })
+  }
+
+  onEscapePress(event: Event) {
+    if (this.route.snapshot.parent?.children.some(child => child.outlet == 'peek')) {
+      this.router.navigate([{ outlets: { peek: [] } }], { relativeTo: this.route.parent })
+    }
   }
 }
