7696ffdf8acd49f44a9a822f35119947905a6252
Sherlock Yiu <1911214+sheryiu@users.noreply.github.com>
2026-06-26 00:15:58 +0800

    feat: breadcrumb helper function now correctly infer deps using InjectionToken

--- projects/portal-ui-ng/components/src/lib/breadcrumbs/breadcrumbs.ts
+++ projects/portal-ui-ng/components/src/lib/breadcrumbs/breadcrumbs.ts
@@ -1,10 +1,11 @@
+import { InjectionToken } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { Observable } from 'rxjs';
 
 const BREADCRUMB_SYMBOL = Symbol('breadcrumb');
 
 type MappedInstanceType<T extends readonly any[]> = {
-  [Key in keyof T]: InstanceType<T[Key]>
+  [Key in keyof T]: T[Key] extends InjectionToken<infer U> ? U : InstanceType<T[Key]>
 }
 
 export type Breadcrumb<Deps extends readonly any[] = any[]> = {
